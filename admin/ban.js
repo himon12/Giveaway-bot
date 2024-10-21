b3f8f386d94f01246e56c736f73faadb7511d80a
himon12 <100027969+himon12@users.noreply.github.com>
2024-10-21 22:51:38 +0545

    Update ban.js

--- admin/ban.js
+++ admin/ban.js
@@ -1,1 +1,16 @@
-admin/ban.js
+module.exports = {
+    name: 'ban',
+    description: 'Ban a user from the server.',
+    permissions: ['BAN_MEMBERS'], // Required permission for this command
+    execute: async (message, args) => {
+        const member = message.mentions.members.first();
+        if (!member) return message.reply('Please mention a valid member of this server.');
+
+        try {
+            await member.ban();
+            message.reply(`${member.displayName} was banned from the server.`);
+        } catch (error) {
+            message.reply('I cannot ban this member.');
+        }
+    },
+};
